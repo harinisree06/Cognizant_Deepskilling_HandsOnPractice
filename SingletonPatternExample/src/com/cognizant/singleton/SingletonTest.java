9abeb66c885fe60ebd520b654f74f5a94bcf74ff
SriKrishnaCollege <harinishreer2006@gmail.com>
2026-07-05 21:34:42 +0530

    Completed Exercise 1 - Singleton Pattern

---
+++ SingletonPatternExample/src/com/cognizant/singleton/SingletonTest.java
@@ -0,0 +1,22 @@
+package com.cognizant.singleton;
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Application Started");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("User Logged In");
+
+        System.out.println("Logger1 HashCode: " + logger1.hashCode());
+        System.out.println("Logger2 HashCode: " + logger2.hashCode());
+
+        if (logger1 == logger2) {
+            System.out.println("Singleton Pattern works!");
+        } else {
+            System.out.println("Singleton Pattern failed!");
+        }
+    }
+}
